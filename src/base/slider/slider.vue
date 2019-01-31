58fdb9f8356d62a2b72b071abbbdc9c397fb0985
carrot0822 <a384713827@qq.com>
2019-02-01 01:53:30 +0800

    slider-Components

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="slider">
-    <div class="slider-group">
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
       <slot>
 
       </slot>
@@ -10,3 +10,100 @@
     </div>
   </div>
 </template>
+
+<script>
+import BScroll from 'better-scroll'
+import {addClass} from '../../common/js/dom'
+export default {
+  props: {
+    loop: {
+      type: Boolean,
+      default : true
+    },
+    autoPlay: {
+      type: Boolean,
+      default: true
+    },
+    interval: {
+      type: Number,
+      default: 4000
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this._setSliderWidth()
+      this._initSlider()
+    },20)
+  },
+  methods: {
+    _setSliderWidth() {
+      this.children = this.$refs.sliderGroup.children // 抓取sliderGroup的子节点
+
+      let width =0;
+      let sliderWidth = this.$refs.slider.clientWidth //抓取slider的宽度
+      for (let i = 0; i < this.children.length; i++ ){
+        let child = this.children[i]
+        addClass(child, 'slider-item') // 给每个child节点添加上这个样式
+
+        child.style.width = sliderWidth + 'px' //使每个child节点的宽度与设置的slider宽度一直
+        console.log(child.style.width)
+        width += sliderWidth
+      }
+
+      if (this.loop) {
+        width += 2*sliderWidth // 如果设置为true 则width为两倍宽度
+      }
+      this.$refs.sliderGroup.style.width = width + 'px' //宽度赋予给外层的sliderGroup 若未设置宽度则不浮动
+      console.log(this.$refs.sliderGroup.style.width)
+    },
+    _initSlider() {
+      this.slider = new BScroll(this.$refs.slider, {
+        scrollX: true,
+        scrollY: false,
+        momentum: false,
+        snap: true,
+        snapLoop: this.loop,
+        snapThreshold: 0.3,
+        snapSpeed: 400,
+        click: true
+      })
+    }
+  }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "../../common/stylus/variable"
+
+  .slider
+    min-height : 1px;
+    .slider-group
+      position : relative;
+      overflow : hidden;
+      white-space : nowrap;
+      .slider-item
+        float : left;
+        box-sizing : border-box;
+        overflow : hidden;
+        text-align : center;
+        a
+          display : block;
+          width : 100%;
+          overflow : hidden;
+          text-decoration : none;
+        img
+          display : block;
+          width : 100%;
+    .dots
+      position : absolute;
+      right : 0;
+      left : 0;
+      bottom : 12px;
+      text-align : center;
+      font-size : 0;
+      width : 20px;
+      border-radius : 5px;
+      background : $color-text-ll;
+
+</style>
--- src/components/recommand/recommand.vue
+++ src/components/recommand/recommand.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper">
-
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="(item,index) in recommends" :key="index">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
@@ -16,10 +22,16 @@
 
 
 <script>
+import Slider from '../../base/slider/slider'
 import {getRecommend} from '../../api/recommend'
 import {ERR_OK} from '../../api/config'
 
 export default {
+  data() {
+    return {
+      recommends : []
+    }
+  },
   created(){
     this._getRecommend()
   },
@@ -27,10 +39,14 @@ export default {
     _getRecommend() {
       getRecommend().then((res) => {
         if(res.code === ERR_OK){
+          this.recommends = res.data.slider
           console.log(res.data.slider)
         }
       })
     }
+  },
+  components: {
+    Slider
   }
 }
 </script>
