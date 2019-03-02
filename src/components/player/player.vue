eaacfa72cc75a1f0b6d0f902f03e30a0448346da
carrot0822 <a384713827@qq.com>
2019-03-03 01:29:54 +0800

    播放功能 前进后退功能完成

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -24,7 +24,7 @@
         <div class="middle">
           <div class="middle-l">
             <div class="cd-wrapper" ref="cdWrapper">
-              <div class="cd">
+              <div class="cd" :class="cdCls">
                 <img class="image" :src="currentSong.image">
               </div>
             </div>
@@ -32,18 +32,25 @@
         </div>
         <!-- 下方按钮 -->
         <div class="bottom">
+          <!-- 进度条和时间 -->
+          <div class="progress-wrapper">
+            <span class="time time-l">{{format(currentTime)}}</span>
+            <div class="progress-bar-wrapper"></div>
+            <span class="time time-r">{{format(currentSong.duration)}}</span>
+          </div>
+          <!-- 操作按钮 -->
           <div class="operators">
             <div class="icon i-left">
-              <i class="icon-sequence"></i>
+              <i  class="icon-sequence"></i>
             </div>
-            <div class="icon i-left">
-              <i class="icon-prev"></i>
+            <div class="icon i-left" :class="disableCls">
+              <i @click="prev" class="icon-prev"></i>
             </div>
             <div class="icon i-center">
-              <i class="icon-play"></i>
+              <i @click="togglePlaying" :class="playIcon"></i>
             </div>
-            <div class="icon i-right">
-              <i class="icon-next"></i>
+            <div class="icon i-right" :class="disableCls">
+              <i @click="next" class="icon-next"></i>
             </div>
             <div class="icon i-right">
               <i class="icon-not-favorite"></i>
@@ -56,17 +63,27 @@
     <transition name="mini">
       <div class="mini-player" v-show="!fullScreen" @click="open">
         <div class="icon">
-          <img width="40" height="40" :src="currentSong.image">
+          <img :class="cdCls" width="40" height="40" :src="currentSong.image">
         </div>
         <div class="text">
           <h2 class="name" v-html="currentSong.name"></h2>
           <p class="desc" v-html="currentSong.singer"></p>
         </div>
         <div class="control">
+          <i @click.stop="togglePlaying" :class="miniIcon"></i>
+        </div>
+        <div class="control">
           <i class="icon-playlist"></i>
         </div>
       </div>
     </transition>
+    <!-- 音频播放 -->
+    <audio ref="audio" 
+    @canplay="ready" 
+    @error="error" 
+    :src="currentSong.url"
+    @timeupdate="updateTime"
+    ></audio>
   </div>
   
 </template>
@@ -78,13 +95,48 @@ import {prefixStyle} from 'common/js/dom'
 
 const transform = prefixStyle('transform')
 export default {
+  data() {
+    return {
+      songReady: false, // 用于控制歌曲是否能够播放
+      currentTime: 0 // 用于存放播放时间
+    }
+  },
   computed: {
+    playIcon() {
+      return this.playing ? 'icon-pause': 'icon-play';
+    },
+    miniIcon() {
+      return this.playing ? 'icon-pause-mini': 'icon-play-mini';
+    },
+    cdCls() {
+      return this.playing ? 'play' : 'play pause'
+    },
+    disableCls() {
+      return this.songReady ? '':'disable'
+    },
     ...mapGetters([
       'fullScreen',
       'playing',
       'currentSong',
-      'playList'
+      'playList',
+      'currentIndex'
     ])
+  },
+  watch:{
+    currentSong() {
+      // 需要等待audio加载完才可以
+      this.$nextTick(()=>{
+        this.$refs.audio.play()
+      })
+    },
+    // 这里才真正的把state的数据与行为绑定在了一起
+    playing(newPlaying) {
+      const audio = this.$refs.audio
+      // 监听playing的状态来修改是否播放 与行为绑定
+      this.$nextTick(()=>{
+        newPlaying? audio.play():audio.pause();
+      })
+    }
   },
   methods: {
     back() {
@@ -94,25 +146,94 @@ export default {
     open() {
       this.setFullScreen(true)
     },
+    // 音乐播放相关功能
+    togglePlaying() {
+      if (!this.songReady) {
+        return
+      }
+      this.setPlayingState(!this.playing) // 对已有的playing状态取反
+    },
+    // 都依赖于播放功能 内容切换 再次播放
+    prev() {
+      if (!this.songReady) {
+        return
+      }
+      let index = this.currentIndex - 1
+      if (index === -1) {
+        index = this.playList.length
+      }
+      this.setCurrentIndex(index)
+      if (!this.playing) {
+        this.togglePlaying() // 如果没有播放 就让他播放
+      }
+      this.songReady = false
+    },
+    next() {
+      // 如果是false就不执行 禁用按钮 直到自定义事件返回true的时候
+      // 手动补了一个异步加载的事件
+      if (!this.songReady) {
+        return
+      }
+      let index = this.currentIndex + 1
+      // 处理边界情况 最后一首的时候
+      if (index === this.playList.length) {
+        index = 0
+      }
+      this.setCurrentIndex(index)
+      if (!this.playing) {
+        this.togglePlaying() // 如果没有播放 就让他播放
+      }
+      // 第一首执行完后调整为false 等待下一次加载事件的循环
+      this.songReady = false
+    },
+    // 额外情况控制
+    ready() {
+      this.songReady = true
+    },
+    error() {
+      this.songReady = true
+    },
+    // 播放时间相关
+    updateTime(e) {
+      this.currentTime = e.target.currentTime
+    },
+    format(interval) {
+      interval = interval | 0
+      const minute = interval / 60 | 0
+      const second = this._pad(interval % 60)
+      return`${minute}:${second}`
+    },
+    // 字符串补0
+    _pad(num, n = 2) {
+      let len = num.toString().length
+      while (len<n) {
+        num = '0' + num
+        len ++
+      }
+      return num
+    },
     ...mapMutations({
-      setFullScreen: 'SET_FULL_SCREEN' // 映射mututaions内的修改数据数据方法拿到此组件使用
+      setFullScreen: 'SET_FULL_SCREEN', // 映射mututaions内的修改数据数据方法拿到此组件使用
+      setPlayingState: 'SET_PLAYING_STATE', // 映射修改状态的数据
+      setCurrentIndex: 'SET_CURRENT_INDEX'
     }),
-    // 动画钩子函数
+    // 动画钩子函数 目的是获得 圆片间XY轴的距离和比例大小 然后利用translated做偏移做动画而非直接改变style
     _getPosAndScale() {
-      const targetWidth = 40
-      const paddingLeft = 40
-      const paddingBottom = 30
-      const paddingTop = 80
-      const width = window.innerWidth * 0.8
+      const targetWidth = 40 // 小圆形唱片的直径
+      const paddingLeft = 40 // 离左侧边界的距离
+      const paddingBottom = 30 // 小圆片离底部边界的距离
+      const paddingTop = 80 // 大圆片离顶部边界的距离
+      const width = window.innerWidth * 0.8 // 此处width为圆形唱片的直径
       const scale = targetWidth / width
-      const x = -(window.innerWidth / 2 -paddingLeft)
-      const y = window.innerHeight - paddingTop - width/2 -paddingBottom
+      const x = -(window.innerWidth / 2 -paddingLeft) // 小圆片与大圆片X轴的距离
+      const y = window.innerHeight - paddingTop - width/2 -paddingBottom // 小圆片与大圆片Y轴距离
       return {
         x,
         y,
         scale
       }
     },
+    // 钩子函数 需要查看库的使用方法
     enter(el, done) {
       const {x, y, scale} = this._getPosAndScale()
 
@@ -232,6 +353,7 @@ export default {
               box-sizing: border-box;
               border: 10px solid rgba(255, 255, 255, 0.1)
               border-radius: 50%;
+              /*CD 旋转的使用方法 */
               &.play
                 animation: rotate 20s linear infinite
               &.pause
@@ -247,6 +369,24 @@ export default {
         position: absolute;
         bottom: 50px;
         width: 100%;
+        .progress-wrapper
+          display: flex
+          align-items: center
+          width: 80%;
+          margin: 0px auto;
+          padding: 10px 0;
+          .time
+            color: $color-text;
+            font-size: $font-size-small;
+            flex: 0 0 30px;
+            line-height: 30px;
+            width: 30px;
+            &.time-l
+              text-align: left;
+            &.time-r
+              text-align: right;
+          .progress-bar-wrapper
+            flex: 1;
         .operators
           display: flex;
           align-items: center;
@@ -341,4 +481,9 @@ export default {
           position: absolute
           left: 0
           top: 0
+  @keyframes rotate
+    0%
+      transform: rotate(0)
+    100%
+      transform: rotate(360deg)
 </style>
